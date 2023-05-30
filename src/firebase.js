7346f8fb6a7a6e53d01d71a4d90347c559f57261
ROBIN <apple@APPLEs-MacBook-Air.local>
2023-05-30 13:29:45 +0530

    set up env

--- src/firebase.js
+++ src/firebase.js
@@ -2,19 +2,19 @@ import { initializeApp } from "firebase/app";
 import {GoogleAuthProvider, getAuth} from "firebase/auth"
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCZvmzMW7cLd5z34IexkMIhgUEDpca0hkc",
-    authDomain: "firechat-8b345.firebaseapp.com",
-    projectId: "firechat-8b345",
-    storageBucket: "firechat-8b345.appspot.com",
-    messagingSenderId: "205369552418",
-    appId: "1:205369552418:web:02a0ac0d4de408fb63e47b",
-    measurementId: "G-SL1ZZZ50PN"
+    apiKey: import.meta.env.VITE_API_KEY,
+    authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_APP_ID,
+    measurementId: import.meta.env.VITE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 
-const provider = new GoogleAuthProvider()
+const provider = new GoogleAuthProvider();
 const auth = getAuth()
 export {provider,app,auth};
